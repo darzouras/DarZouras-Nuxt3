b4f939e1f517dd5702ccdda4258a0a19f6a4225b
Darlene Zouras <dzouras@gmail.com>
2022-04-23 17:00:53 -0500

    update: storyblok connection established

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  components: {
+    global: true,
+    dirs: ['~/components/storyblok']
+  },
   build: {
     postcss: {
       postcssOptions: {
@@ -15,4 +19,10 @@ export default defineNuxtConfig({
   head: {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
+  runtimeConfig: {
+    public: {
+      sbAccessToken: process.env.STORYBLOK_ACCESS_TOKEN,
+      isPreview: process.env.PREVIEW,
+    },
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
+<script setup>
+import { useStoryblok } from '@storyblok/vue'
+
+const config = useRuntimeConfig()
+const story = await useStoryblok('home', {
+  version: config.public.isPreview ? 'draft' : 'published'
+})
+</script>
+
 <template>
-  <main class="container bg-mid-green text-dark-purple">:)</main>
+  <main class="container bg-mid-green text-dark-purple">{{ story }}</main>
 </template>
