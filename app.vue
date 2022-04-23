8de4e3ffe95d70d3cd4d51021f5931e7795b4a99
Darlene Zouras <dzouras@gmail.com>
2022-04-23 10:08:46 -0500

    initial commit
    also tailwind setup

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLayout name="default">
+    Hi this is App.vue
+  </NuxtLayout>
+</template>
+
+<script setup>
+  import '~/assets/css/main.css'
+</script>
---
+++ layouts/Default.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="bg-black text-white p-10">
+        <div>THIS IS THE TEMPLATE</div>
+        <slot />
+    </div>
+</template>
