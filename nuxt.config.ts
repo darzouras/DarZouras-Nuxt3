0791c633e8d10e09f1c1e5fc3e61877e091a8a93
Darlene Zouras <dzouras@gmail.com>
2022-04-23 11:21:13 -0500

    update: trying to set favicon?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,9 @@ export default defineNuxtConfig({
             }
         },
     },
+    head: {
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    }
 })
